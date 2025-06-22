ffe3f1485a3ee29a974b879bbcc607d7675f19eb
Zack <zbostock56@gmail.com>
2025-06-21 20:53:36 -0400

    fixed gdb issue

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -21,11 +21,10 @@ if ! [ -d "./.gdb" ]; then
     mkdir ./.gdb
 fi
 
-
 # Generate the GDB script
 cat > .gdb/.gdb_script.gdb << EOF
     file kernel/bin/kernel
-    add-symbol-file userspace/bin/hsh
+    add-symbol-file userspace/bin/hsh 0xffff800040000000
 EOF
 
 # If GEF is installed, add remote-safe GEF commands
--- userspace/src/hsh.c
+++ userspace/src/hsh.c
@@ -526,6 +526,7 @@ CMD *parse_cmd(char *s) {
 }
 
 int main() {
+    printf("Shell started...\n");
     char *buf = (char *)(malloc(CMD_MAX_LEN));
     if (!buf) {
         panic("hsh: failed to allocate memory for the command buffer\n");
